dcc45f833561e5bcbfb94e1f71cad857b1e29735
Athar Raza Saeedi <atharaza.dev@gmail.com>
2024-01-14 01:37:10 +0500

    Update HomePage.js

--- frontend/src/pages/Website/Home Page/HomePage.js
+++ frontend/src/pages/Website/Home Page/HomePage.js
@@ -21,23 +21,7 @@ function HomePage() {
 
             {/* ======================================== Hero Section ======================================== */}
             <section className="relative  overflow-hidden bg-gradient-to-b from-blue-100 via-transparent to-transparent xl:pt-28">
-                <div className="relative z-10">
-                    <div className="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 justify-center overflow-hidden [mask-image:radial-gradient(50%_45%_at_50%_55%,white,transparent)]">
-                        <svg className="h-[60rem] w-[100rem] flex-none stroke-blue-600 opacity-20" aria-hidden="true">
-                            <defs>
-                                <pattern id="e9033f3e-f665-41a6-84ef-756f6778e6fe" width="200" height="200" x="50%" y="50%"
-                                    patternUnits="userSpaceOnUse" patternTransform="translate(-100 0)">
-                                    <path d="M.5 200V.5H200" fill="none"></path>
-                                </pattern>
-                            </defs>
-                            <svg x="50%" y="50%" className="overflow-visible fill-blue-50">
-                                <path d="M-300 0h201v201h-201Z M300 200h201v201h-201Z" stroke-width="0"></path>
-                            </svg>
-                            <rect width="100%" height="100%" stroke-width="0" fill="url(#e9033f3e-f665-41a6-84ef-756f6778e6fe)">
-                            </rect>
-                        </svg>
-                    </div>
-                </div>
+            
                 <div className="relative pt-24 sm:py-16 lg:pt-0 xl:pb-0">
                     <div className="relative mx-auto max-w-8xl px-4 sm:px-6 lg:px-8">
                         <div className="mx-auto max-w-6xl text-center">
