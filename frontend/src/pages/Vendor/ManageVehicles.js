6d2c23c99a51398f9dcb6723be91dc80a4b0cb37
Athar Raza Saeedi <atharaza.dev@gmail.com>
2024-02-20 13:44:08 +0500

    updated

--- frontend/src/pages/Vendor/ManageVehicles.js
+++ frontend/src/pages/Vendor/ManageVehicles.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function ManageVehicles() {
-  return (
-    <div>ManageVehicles</div>
-  )
+    return (
+        <>
+            <div class="w-full h-full overflow-x-auto fontAlt bg-white rounded-lg shadow-sm border-1 border-gray-250 mt-2">
+
+                <div class=" py-4 md:py-7 px-4 md:px-8 xl:px-10">
+                    <h1 class="fontAlt sm:text-6xl mb-8 font-bold text-start text-primColor1">Manage Vehicles</h1>
+        
+
+                </div>
+
+            </div >
+        </>
+    )
 }
 
 export default ManageVehicles
